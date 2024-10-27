d1d42d26cb3aa5494ab850b7f0ae3002b1f68ab0
chuang091 <thomas@ultralytics.com>
2024-10-27 17:53:17 +0800

    chore: init map

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,27 @@
-// return hello world
-export default function Map() {
-    return (
-        <div>
-        <h1>Map</h1>
-        </div>
-    );
-    }
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// 設定 Marker 的圖標
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+const MapComponent = () => {
+  const position = [25.0330, 121.5654]; // 台北市的經緯度
+
+  return (
+    <MapContainer center={position} zoom={15} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://gis.sinica.edu.tw/tileserver/file-exists.php?img=JM20K_1921-jpg-{z}-{x}-{y}"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
