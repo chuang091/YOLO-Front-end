394ad475fdf2de2787eff93460a4aa7dbed65591
chuang091 <thomas@ultralytics.com>
2024-11-03 23:44:07 +0800

    Refactor tag.ts: Remove console.log statement and add missing newline at end of file

--- src/tag.ts
+++ src/tag.ts
@@ -8,7 +8,6 @@ interface Tag {
   }
   
   export const handleTagSelectedImages = (selectedImages: Image[], openModal: () => void): void => {
-    console.log("選擇的圖片:", selectedImages);
     openModal(); // 確保這裡的 openModal 是從父組件傳入的有效函數
   };
   
